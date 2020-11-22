b323e5dd94c96fffaa80e5282cb1b0b0a2d30ad7
NguyenPhuoc1207 <72172378+NguyenPhuoc1207@users.noreply.github.com>
2020-11-22 21:12:46 +0700

    Add files via upload

---
+++ cautruclapvarenhanh.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=float(input("a="))
+b=float(input("b="))
+if a==0:
+    b==0
+    print("phuong trinh co vo so nghiem")
+else:
+    x=-b/a
+    counter=1
+
+    n=int(input("n="))
+
+    while counter <= n:
+     print("phuong trinh co nghiem la",x,counter)    
+counter+=1
+print("ket thuc")
+
+
+            
+        
+    
+    
